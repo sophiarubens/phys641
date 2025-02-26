9a1576c4b893a7cfe900dddfe5aca9ccb9ee561a
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-02-26 16:12:42 -0500

    waypoint halfway through trying to downsample

--- chime2.py
+++ chime2.py
@@ -209,17 +209,19 @@ def SNR(Spulsar,N,Ssys):
     return Spulsar*np.sqrt(N)/Ssys
 
 SNR1pulse=SNR(Spulsar,1,bskmasked)
-plt.figure()
-plt.plot(calfreqs,SNR1pulse)
 snr1mean=np.nanmean(SNR1pulse)
 snr1max=np.nanmax(SNR1pulse)
 snr1min=np.nanmin(SNR1pulse)
 
-plt.xlabel('frequency (MHz)')
-plt.ylabel('dimensionless, unitless SNR proportionality')
-plt.title('RHS of SNR proportionality for N=1 and the POWER LAW pulsar spectrum')
-plt.savefig('one_pulse_theo_snr.png')
-plt.show()
+plottheospec=False
+if plottheospec:
+    plt.figure()
+    plt.plot(calfreqs,SNR1pulse)
+    plt.xlabel('frequency (MHz)')
+    plt.ylabel('dimensionless, unitless SNR proportionality')
+    plt.title('RHS of SNR proportionality for N=1 and the POWER LAW pulsar spectrum')
+    plt.savefig('one_pulse_theo_snr.png')
+    plt.show()
 
 meanSNR1pulse=np.nanmean(SNR1pulse)
 N_from_mean=(2./snr1mean)**2 # factor_by_which_you_need_to_increase_snr=2./meanSNR1 = sqrt(N_required) -> N_required = (2./meanSNR1)**2
@@ -231,6 +233,18 @@ print('Based on the max  one-pulse SNR,',np.round(snr1max,4),', x=',round(N_from
 print('Based on the min  one-pulse SNR,',np.round(snr1min,4),', x=',round(N_from_min))
 
 # 2. load and normalize pulsar data
+# pfil=FilReader('pulsardata.fil')
+# _, pmask=pfil.clean_rfi(method='mad',threshold=3.) ## PREVIOUSLY, I WAS NOT SPECIFYING mask_value
+# pfil=FilReader('pulsardata_masked.fil') # use the RFI-flagged version of the data
+# pns=pfil.header.nsamples # number of samples
+# pfileblock=pfil.read_block(0,pns) # data is accessible by reading a block
+# pfiledata=pfileblock.data
+# pulsarspec_adu=np.mean(pfiledata,axis=1) # average over phase to get the spectral data as a function of frequency
+# pspecaduzero=np.nonzero(pulsarspec_adu==0)
+# pulsarspec_adu[pspecaduzero]=np.nan
+
+# # transfer[np.nonzero(transfer==0)]=np.nan # try this to see if it helps remove the weird drops down to zero in the #2 SNR plot that show up even though I did RFI flagging
+
 pfil=FilReader('pulsardata.fil')
 _, pmask=pfil.clean_rfi(method='mad',threshold=3.,mask_value=0) ## PREVIOUSLY, I WAS NOT SPECIFYING mask_value
 pfil=FilReader('pulsardata_masked.fil') # use the RFI-flagged version of the data
@@ -253,27 +267,32 @@ pspecaduzero=np.nonzero(pulsarspec_adu==0)
 pulsarspec_adu[pspecaduzero]=np.nan
 
 transfer[np.nonzero(transfer==0)]=np.nan # try this to see if it helps remove the weird drops down to zero in the #2 SNR plot that show up even though I did RFI flagging
-pulsarspec_jy=pulsarspec_adu*transfer # pulsar spectrum in physical units (Janskys)
-
-plt.figure()
-plt.plot(calfreqs,pulsarspec_jy)
-plt.xlabel('freq (MHz)')
-plt.ylabel('spectral flux density (Jy)')
-plt.title('pulsar flux check')
-plt.show()
 
 checkfreqs=False
 if checkfreqs:
     print("pfreqs==calfreqs?",(pfreqs==calfreqs).all())
     print('calfreqs==bskfreqs?',(calfreqs==bskfreqs).all()) # since this prints out True, there's no need to worry about different spectra originally prepared at the calibrator / blank sky frequencies having different frequency values for the same array indices ... the sort of less hacky way to do this would have been to check that the f0, df, and nchan were all the same
-SNRdataversion=SNR(pulsarspec_jy,1,bskmasked) # SNR(Spulsar,N,Ssys) # once again, the pulsar SNR, but this time using the data spectrum, not the theoretical spectrum
 
-pfileblock.normalise()  # normalize data within each channel
+################
+pfil.downsample(tfactor=32)
+pfil32=FilReader('pulsardata_masked_f1_t32.fil') # off_Fil_32 = FilReader("/home/jovyan/work/phys641data/Data/blank_sky_masked_f1_t32.fil") 
+p32block=pfil32.read_block(0,pfil32.header.nsamples, pfil32.header.fch1, pfil32.header.nchans) # off_data_32 = off_Fil_32.read_block(0, off_Fil_32.header.nsamples, off_Fil_32.header.fch1, off_Fil_32.header.nchans)
+pnormed=p32block.normalise()  # normalize data within each channel
+################
+
+# pnormed=pfileblock.normalise()
 print('done normalizing')
-pfiledata=pfileblock.data
-pulsarspec_adu=np.mean(pfiledata,axis=1) # average over phase to get the spectral data as a function of frequency
-pspecaduzero=np.nonzero(pulsarspec_adu==0)
-pulsarspec_adu[pspecaduzero]=np.nan
+pnormeddata=pnormed.data
+
+plt.figure(figsize=(10,5))
+plt.imshow(pnormeddata,extent=[bsktimes0[0],bsktimes0[-1],bskfreqs[-1],bskfreqs[0]],aspect=1e-2,vmin=np.percentile(pnormeddata,1),vmax=np.percentile(pnormeddata,99))
+plt.xlabel('time (s) after '+calt0iso)
+plt.ylabel('frequency (MHz)')
+plt.title('Normalized pulsar data')
+cbar=plt.colorbar()
+cbar.set_label('Flux (ADU)')
+plt.tight_layout()
+plt.show()
 
 # 3. DM transform
 fterm=1/(400.**2)-1/(800.**2) # GHz; tau = kDM*DM*fterm so deltatau = kDM*deltaDM*fterm -> deltaDM = deltatau/(kDM*fterm)
@@ -283,7 +302,7 @@ deltaDM=pdt/(kDM*fterm) # pc cm**{-3} **desired spacing for the dm search
 print('deltaDM=',deltaDM)
 DMrange=2*ctrdm # want to search 10 to 50, but dmt_transform searches 0 to 2*dm, and I'd rather have symmetric regions outside the most likely one (because I don't know if an abnormally low or high dm is more likely), so I call with 30 as the center and not 25 or anything else
 ndmsteps=8
-dm_t=pfileblock.dmt_transform(ctrdm,dmsteps=ndmsteps) # massively scaled down version that runs quickly (useful for figuring out the rest of the steps)
+dm_t=pnormed.dmt_transform(ctrdm,dmsteps=ndmsteps) # massively scaled down version that runs quickly (useful for figuring out the rest of the steps)
 # ndmsteps=int(DMrange/deltaDM)
 # print('number of DM steps to take at the minimum sensible spacing =',ndmsteps)
 # dm_t=pfileblock.dmt_transform(ctrdm,dmsteps=ndmsteps) # full call that takes 40ish minutes to run
@@ -296,25 +315,32 @@ plt.figure(figsize=(10,5))
 plt.imshow(dm_t.data,extent=[ptimes0[0],ptimes0[-1],2*ctrdm,0],aspect=0.1) # L,R,B,T
 cbar=plt.colorbar()
 cbar.set_label('S/N')
-plt.xlabel('frequency (Hz), calculated from time (s) after '+pt0iso)
+plt.xlabel('time (s) after '+pt0iso)
 plt.ylabel('DM ')
 plt.title('DM-time grid')
 plt.show()
 
 # 4. search for periodic signal in Fourier space
-dm_f=np.fft.rfft(dm_t.data)
+dm_f=np.fft.rfft(dm_t.data).real # we don't care about the imag part bc the rfft of a real-valued array is purely real (no risk of losing info here)
 pfreqs=np.fft.fftfreq(pns,d=pdt) # as many frequencies as number of time samples, from a time array with spacing pdt
 plt.figure(figsize=(10,5))
-plt.imshow(dm_f,extent=[pfreqs[0],pfreqs[-1],2*ctrdm,0],aspect=0.1)
+plt.imshow(dm_f,extent=[pfreqs[0],pfreqs[-1],2*ctrdm,0],aspect=1e-3,norm='log',vmax=np.percentile(dm_f,99))
 cbar=plt.colorbar()
 cbar.set_label('S/N')
 plt.xlabel('frequency (Hz), calculated from time (s) after '+pt0iso)
 plt.ylabel('DM')
 plt.title('DM-frequency grid')
 plt.show()
+bestloc=np.unravel_index(dm_f.argmin(), dm_f.shape)
+print('maximal SNR is',dm_f[bestloc])
+best_freq=pfreqs[bestloc[0]]
+print('SNR-maximizing freq is',best_freq)
+best_dm=bestloc[1]*deltaDM
+print('SNR-maximizing DM is',best_dm)
+assert(1==0)
 
 # 5. fold data to find pulse
-
+pfolded=pfil.fold(best_freq,best_dm,nints=1,nbands=1024,nbins=int(0.5//pdt)) # positional args are ordered as period,dm
 # off_data_32_folded = off_Fil_32.fold(0.5,2,nints=1,nbands=1024,nbins=int(0.5//off_Fil_32.header.tsamp))
 # plt.figure()
 # plt.imshow(off_data_32_folded.data[0,:,:],aspect='auto',interpolation='nearest')
@@ -322,3 +348,5 @@ plt.show()
 # plt.xlabel('Time [samples]')
 # plt.ylabel('Freq. [channel]')
 # plt.show()
+
+# Tldr upshot of a 208 discussion: internally, dmt_transform creates dm_arr = dm + np.linspace(-dm, dm, dmsteps), so manipulate your Δdm accordingly
