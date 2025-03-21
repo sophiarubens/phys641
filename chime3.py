994ad37ff62a0710740188828fd0d9c579c8fd42
Sophia Rubens <116817240+sophiarubens@users.noreply.github.com>
2025-03-21 15:28:59 -0400

    Misc SNR meddling

--- chime3.py
+++ chime3.py
@@ -31,13 +31,19 @@ blank_masked_downsampled_normalized=blank_maskedblock.normalise()
 
 # PERSONALIZED DATA
 id_of_interest=261215947 # me
-# id_of_interest=261213158 # test w/ a random classmate's data
+# id_of_interest=261209827 # Kim
+# id_of_interest=261215346 # Zach
+# id_of_interest=0 # placeholder (want to leave my code the same-ish when inspecting the injection)
 data3=FilReader('data_'+str(id_of_interest)+'.fil')
+# data3=FilReader('data_w_injected_sig.fil')
 _, data3mask=data3.clean_rfi(method='mad',threshold=3.)
 data3_masked=FilReader('data_'+str(id_of_interest)+'_masked.fil') # use the RFI-flagged version of the data
+# data3_masked=FilReader('data_w_injected_sig_masked.fil')
+data3_non_downsampled_dt=data3_masked.header.tsamp
 
 data3_masked.downsample(tfactor=tfac) # downsample by a factor of tfactor
 data3_masked_downsampled=FilReader('data_'+str(id_of_interest)+'_masked_f1_t'+str(tfac)+'.fil')
+# data3_masked_downsampled=FilReader('data_w_injected_sig_masked_f1_t'+str(tfac)+'.fil')
 data3head=data3_masked_downsampled.header
 data3_t0=data3head.tstart
 data3_dt=data3head.tsamp
@@ -75,50 +81,81 @@ fterm=1/(400.**2)-1/(800.**2) # GHz; tau = kDM*DM*fterm so deltatau = kDM*deltaD
 kDM=4148.8 # MHz**2 pc**{-1} cm**3 s
 deltaDM=data3_dt/(kDM*fterm) # pc cm**{-3} **desired spacing for the dm search
 dm_lo=-50
+# dm_lo=-100
 dm_hi=0
 DMrange=dm_hi-dm_lo 
 n_dms=int(DMrange/deltaDM)
 dm_candidates=np.linspace(dm_lo,dm_hi,n_dms) # use DM magnitudes up to the edge of the galaxy, but negative
 
-max_width=1. # cap at 1 s for now
-n_widths= 25 # don't expect to see anything narrower than one time sample, but that makes the number of loop iterations too large
-width_candidates=np.linspace(data3_dt,max_width,n_widths) 
+max_width=0.1 # cap at 1 s for now
+coarse_width_search=False
+if coarse_width_search: # hard-coded smaller parameter set to speed the search
+    n_widths= 25
+    width_candidates=np.linspace(data3_non_downsampled_dt,max_width,n_widths) 
+else: # more compute time but more accurate to the limits of the survey
+    width_candidates=np.arange(data3_non_downsampled_dt,max_width,data3_non_downsampled_dt)
+    n_widths=len(width_candidates)
+    print('n_widths=',n_widths)
 
 dm_width_array=np.zeros((n_dms,n_widths)) # max value for best-fit DM and width
 start_index_array=np.zeros((n_dms,n_widths)) # stored values = indices of start time where the correlation is strongest
+# snr_array=np.zeros((n_dms,n_widths))
 
 ##### SEARCH THE DM-WIDTH PARAMETER SPACE
+n_hist_bins=75
 for i,test_dm in enumerate(dm_candidates): # consider the DM candidates
     dedispersed_data=data3_masked_downsampled_normalized.dedisperse(test_dm) # get 2D array dedispersed by the candidate amount
     dedispersed_pulse_profile=dedispersed_data.get_tim().data
+    # dedispersed_blank=blank_masked_downsampled_normalized.dedisperse(test_dm)
+    # dedispersed_blank_profile=dedispersed_blank.get_tim().data
 
     for j, test_width in enumerate(width_candidates): # consider the width candidates
-        data3times_current_template=pulse_template(test_width,data3_times0) # width,times
-        convolved_data3=np.convolve(data3times_current_template,dedispersed_pulse_profile)
+        current_template=pulse_template(test_width,data3_times0) # width,times
+        convolved_data3=np.convolve(current_template,dedispersed_pulse_profile)
+        # ##
+        # convolved_blank=np.convolve(current_template,dedispersed_blank_profile)
+        # data3_hist,data3_bin_edges=np.histogram(convolved_data3,bins=n_hist_bins)
+        # data3_hist_mean=data3_hist@data3_bin_edges[:-1]/np.sum(data3_hist)
+        # blank_hist,blank_bin_edges=np.histogram(convolved_blank,bins=n_hist_bins)
+        # blank_hist_mean=blank_hist@blank_bin_edges[:-1]/np.sum(blank_hist)
+        # ##
 
         maxidx=np.argmax(convolved_data3)
         start_index_array[i,j]=maxidx
         dm_width_array[i,j]=convolved_data3[maxidx]
+        # snr_array[i,j]=data3_hist_mean/blank_hist_mean
 
 ##### VISUALIZE LOOP DATA PRODUCTS
-fig,axs=plt.subplots(1,2,figsize=(10,5))
-im=axs[0].imshow(start_index_array,aspect=0.02,extent=[width_candidates[0],width_candidates[-1],dm_candidates[-1],dm_candidates[0]]) # # extent=[left,right,bottom,top]
+loop_aspect=2e-3
+fig,axs=plt.subplots(1,2,figsize=(20,5))
+im=axs[0].imshow(start_index_array,aspect=loop_aspect,extent=[width_candidates[0],width_candidates[-1],dm_candidates[-1],dm_candidates[0]]) # # extent=[left,right,bottom,top]
 plt.colorbar(im,ax=axs[0])
 axs[0].set_xlabel('width')
 axs[0].set_ylabel('DM')
-axs[0].set_title('start index map')
-im=axs[1].imshow(dm_width_array,aspect=0.02,extent=[width_candidates[0],width_candidates[-1],dm_candidates[-1],dm_candidates[0]])
+axs[0].set_title('start index')
+im=axs[1].imshow(dm_width_array,aspect=loop_aspect,extent=[width_candidates[0],width_candidates[-1],dm_candidates[-1],dm_candidates[0]])
 plt.colorbar(im,ax=axs[1])
 axs[1].set_xlabel('width')
 axs[1].set_ylabel('DM')
-axs[1].set_title('param strength map')
+axs[1].set_title('signal')
+# im=axs[2].imshow(,aspect=loop_aspect,extent=[width_candidates[0],width_candidates[-1],dm_candidates[-1],dm_candidates[0]])
+# plt.colorbar(im,ax=axs[2])
+# axs[3].set_xlabel('width')
+# axs[3].set_ylabel('DM')
+# axs[3].set_title('noise')
+# im=axs[3].imshow(snr_array,aspect=loop_aspect,extent=[width_candidates[0],width_candidates[-1],dm_candidates[-1],dm_candidates[0]])
+# plt.colorbar(im,ax=axs[3])
+# axs[3].set_xlabel('width')
+# axs[3].set_ylabel('DM')
+# axs[3].set_title('SNR')
+plt.tight_layout()
 plt.show()
 
 ##### RESULTING PULSE
 alien_dm_idx,alien_width_idx=np.unravel_index((np.abs(dm_width_array)).argmax(), dm_width_array.shape)
 alien_dm=dm_candidates[alien_dm_idx]
 alien_width=width_candidates[alien_width_idx]
-alien_start_time_idx=int(start_index_array[alien_dm_idx,alien_width_idx])
+alien_start_time_idx=int(start_index_array[alien_dm_idx,alien_width_idx]) # FIGURE OUT OFFSET
 alien_start_time=data3_times0[alien_start_time_idx]
 print('alien signal properties:')
 print('start time=',alien_start_time)
@@ -145,21 +182,22 @@ alien_template=pulse_template(alien_width,data3_times0) # width,times
 convolved_data3=np.convolve(alien_template,alien_pulse_profile)
 convolved_blank=np.convolve(alien_template,blank_pulse_profile)
 
-n_hist_bins=75
 data3_hist,data3_bin_edges=np.histogram(convolved_data3,bins=n_hist_bins)
 data3_hist_mean=data3_hist@data3_bin_edges[:-1]/np.sum(data3_hist)
 blank_hist,blank_bin_edges=np.histogram(convolved_blank,bins=n_hist_bins)
 blank_hist_mean=blank_hist@blank_bin_edges[:-1]/np.sum(blank_hist)
 
-fig,axs=plt.subplots(1,2,figsize=(10,5))
-axs[0].stairs(data3_hist,data3_bin_edges,label='histogram')
-axs[0].axvline(data3_hist_mean,linestyle='--',label='mean')
+fig,axs=plt.subplots(2,1,figsize=(5,10),sharex=True)
+axs[0].stairs(data3_hist,data3_bin_edges,label='histogram',fill=True)
+axs[0].axvline(data3_hist_mean,linestyle='--',c='C1',label='mean')
+axs[0].set_yscale('log')
 axs[0].set_xlabel('significance')
 axs[0].set_ylabel('number of instances')
 axs[0].set_title('data for ID'+str(id_of_interest))
 axs[0].legend()
-axs[1].stairs(blank_hist,blank_bin_edges,label='histogram')  
-axs[1].axvline(blank_hist_mean,linestyle='--',label='mean')          
+axs[1].stairs(blank_hist,blank_bin_edges,label='histogram',fill=True)  
+axs[1].axvline(blank_hist_mean,linestyle='--',c='C1',label='mean')  
+axs[1].set_yscale('log')        
 axs[1].set_xlabel('significance')
 axs[1].set_ylabel('number of instances')
 axs[1].set_title('blank sky')
